8c8cfc378ce58e773f93c70c3601fd113197f7f1
gastondigilio <gastondigilio@gmail.com>
2021-12-24 19:08:30 -0300

    nuevo login

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,18 +2,66 @@ import './home.css';
 import React from 'react';
 import Navbar from "../navBar/navBar";
 import NavBarVertical from "../navBarVertical/navBarVertical";
+import { Form, Row, Col, FormGroup, Button, Label, Input } from 'reactstrap';
 
-export default function Home () {
-    return(
+export default function Home() {
+    return (
         <div>
             <Navbar />
             <div className="navBarVerticalContainer">
-            <NavBarVertical />
+                <NavBarVertical />
             </div>
-        <div className="contenedor">
-            <h4>Bienvenido</h4>
+            <div className="contenedor">
+                <h4>Bienvenido</h4>
+                {/* <div className="container">
+                    <div className="containerGrid1">
+                        <label>X vuelos totales</label>
+                        <br></br>
+                        <br></br>
+                        <NavLink
+                            active
+                            href="#"
+                        >
+                            Más info
+                        </NavLink>
+                    </div>
+                    <div className="containerGrid2">
+                        <label>X hs de vuelo</label>
+                        <br></br>
+                        <br></br>
+                        <NavLink
+                            active
+                            href="#"
+                        >
+                            Más info
+                        </NavLink>
+                    </div>
+                    <div className="containerGrid3">
+                        <label>X hs de vuelo este mes</label>
+                        <br></br>
+                        <br></br>
+                        <NavLink
+                            active
+                            href="#"
+                        >
+                            Más info
+                        </NavLink>
+                    </div>
+                    <div className="containerGrid4">
+                        <label>X próximos turnos</label>
+                        <br></br>
+                        <br></br>
+                        <NavLink
+                            active
+                            href="#"
+                        >
+                            Más info
+                        </NavLink>
+                    </div>
+                </div> */}
+                
         </div>
-        </div>
+        </div >
     )
 
 }
