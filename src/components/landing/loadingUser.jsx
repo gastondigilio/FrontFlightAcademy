759f57a958d1d95f1853bdbb7cfd05d0d060bf45
Abril♥ <abrilmartinagil@gmail.com>
2022-01-31 17:41:11 -0300

    Update loadingUser.jsx

--- src/components/landing/loadingUser.jsx
+++ src/components/landing/loadingUser.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
 
-export default function LoadingUser({ status, setIslogin }) {
-    const navigate= useNavigate();
-    const userLogin = useSelector((state) => state.userLogin)
-
-        if (status) {
-            setIslogin(false)
-            if (userLogin.hasOwnProperty("id")) {
-                navigate('/home')
-            } else {
-                console.log("no hay poriedad id, entro al else")
-            }
-        }
-
+export default function LoadingUser({ status }) {
     return (
         <div>
             {
